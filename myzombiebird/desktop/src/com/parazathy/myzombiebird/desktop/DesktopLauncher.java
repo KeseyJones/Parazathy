62ed67b7ea3b13556fa7168c6e9f935c361b44f4
LuisFer <lfaranguez@hotmail.com>
2014-10-04 18:06:03 +0200

    Zombie Project
    Añadir Zombie Project and Asset de ambos

---
+++ myzombiebird/desktop/src/com/parazathy/myzombiebird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.parazathy.myzombiebird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.parazathy.myzombiebird.MyZombieBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyZombieBird(), config);
+	}
+}
