344e3def476d7081d50a8326e53594a223c77ea4
LuisFer <lfaranguez@hotmail.com>
2014-10-04 14:02:41 +0200

    Mygemas
    Mygemas

---
+++ mygemas/desktop/src/com/parazathy/mygemas/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.parazathy.mygemas.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.parazathy.mygemas.MyGemas;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGemas(), config);
+	}
+}
