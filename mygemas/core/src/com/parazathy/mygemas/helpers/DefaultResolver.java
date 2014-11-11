21d44777c494888761978c80354499f652ab9be7
LuisFer <lfaranguez@hotmail.com>
2014-11-11 21:05:25 +0100

    Arreglado acentos

--- mygemas/core/src/com/parazathy/mygemas/helpers/DefaultResolver.java
+++ mygemas/core/src/com/parazathy/mygemas/helpers/DefaultResolver.java
@@ -18,11 +18,11 @@ public class DefaultResolver implements PlatformResolver {
 	
 	@Override
 	public BitmapFont loadFont(String fntFile, String ttfFile, int size) {
-		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(ttfFile));
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(ttfFile));		
 		FreeTypeFontParameter param = new FreeTypeFontParameter();
 		param.size = size;
 		param.characters = FreeTypeFontGenerator.DEFAULT_CHARS;
-		param.flip = true;
+		param.flip = true;				
 		BitmapFont font = generator.generateFont(param);
 		generator.dispose();
 		return font;
