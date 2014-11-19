3e13f60a915fb9dd3d591f80b1a99a8a353730f6
Luis Fernando Aranguez <lfaranguez@hotmail.com>
2014-11-19 17:47:19 +0100

    Arreglado button

--- mygemas/core/src/com/parazathy/mygemas/ui/MenuButton.java
+++ mygemas/core/src/com/parazathy/mygemas/ui/MenuButton.java
@@ -3,6 +3,7 @@ package com.parazathy.mygemas.ui;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont.TextBounds;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.parazathy.mygemas.helpers.AssetLoader;
 
 public class MenuButton extends Actor {
@@ -11,7 +12,8 @@ public class MenuButton extends Actor {
 	private static final int MENU_GAP_HEIGHT = 100;
 	
 	private String text;
-	private int position;
+	private int position;	
+	private TextBounds bounds;
 	private float widthWorld;
 	private static int selectedOption = -1;
 	private static boolean readyToChange = false;
@@ -20,21 +22,23 @@ public class MenuButton extends Actor {
 		this.text = text;
 		this.position = position;
 		this.widthWorld = widthWorld;
+		this.bounds = AssetLoader.fontMenu.getBounds(text);
+		this.setBounds((widthWorld - this.bounds.width)/2, MENU_START_HEIGHT + position * MENU_GAP_HEIGHT, this.bounds.width, this.bounds.height+4);
+		this.setTouchable(Touchable.enabled);
 		
 	}
     
     @Override
-    public void draw(Batch batch, float alpha){
-    	TextBounds bounds = AssetLoader.fontMenu.getBounds(text);
+    public void draw(Batch batch, float alpha){    	
     	
     	AssetLoader.fontMenu.setColor(0.0f, 0.0f, 0.0f, 0.5f);
-    	AssetLoader.fontMenu.draw(batch, text, (this.widthWorld - bounds.width) / 2, MENU_START_HEIGHT + position * MENU_GAP_HEIGHT + 4);
+    	AssetLoader.fontMenu.draw(batch, text, this.getX(), this.getY() + 4);
     	AssetLoader.fontMenu.setColor(1.0f, 1.0f, 1.0f, 1.0f);
-    	AssetLoader.fontMenu.draw(batch, text, (this.widthWorld - bounds.width) / 2, MENU_START_HEIGHT + position * MENU_GAP_HEIGHT);
+    	AssetLoader.fontMenu.draw(batch, text, this.getX(), this.getY());
     	
     	if (readyToChange && position == selectedOption) {
 			batch.draw(AssetLoader.imgHighlightMenu,
-		    		   ( this.getStage().getWidth() - AssetLoader.imgHighlightMenu.getRegionWidth()) / 2,
+		    		   ( this.widthWorld - AssetLoader.imgHighlightMenu.getRegionWidth()) / 2,
 		    		   MENU_START_HEIGHT + 5 + selectedOption * MENU_GAP_HEIGHT);
 		    		   
 		}		
